84a3910cf03e24d4da52f237be98e3e34501b541
ValentinaZussino <vzussino@protonmail.com>
2022-11-10 17:57:38 +0100

    inizio impostaz footer

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,6 +1,11 @@
 <template>
-    <footer class="container">
-        Footer here
+    <footer >
+        <div class="top-footer">
+            <div class="container"></div>
+        </div>
+        <div class="bottom-footer">
+            <div class="container"></div>
+        </div>
     </footer>
 </template>
 
@@ -11,6 +16,13 @@
 </script>
 
 <style lang="scss" scoped>
-
-
+@use '../assets/styles/partials/variables' as *;
+    .top-footer {
+        background-image: url('../assets/img/footer-bg.jpg'); 
+        background-size: cover;
+        height: 300px;
+    }
+    .bottom-footer {
+        background-color: $darkgray;
+    }
 </style>

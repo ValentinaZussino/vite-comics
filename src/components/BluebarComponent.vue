f8d2ffd866899503528b1f08b056062be851db15
ValentinaZussino <vzussino@protonmail.com>
2022-11-10 19:12:28 +0100

    impostaz bottom-footer

--- src/components/BluebarComponent.vue
+++ src/components/BluebarComponent.vue
@@ -8,7 +8,33 @@
 
 <script>
     export default {
-        name: 'BluebarComponent'
+        name: 'BluebarComponent',
+        data(){
+            return {
+                linksImage : [
+                    {
+                        text: 'digital comics',
+                        url: '../assets/img/buy-comics-digital-comics.png'
+                    },
+                    {
+                        text: 'dc merchandise',
+                        url: '../assets/img/buy-comics-merchandise.png'
+                    },
+                    {
+                        text: 'subscription',
+                        url: '../assets/img/buy-comics-subscriptions.png'
+                    },
+                    {
+                        text: 'comic shop locator',
+                        url: '../assets/img/buy-comics-shop-locator.png'
+                    },
+                    {
+                        text: 'dc power visa',
+                        url: '../assets/img/buy-dc-power-visa.svg'
+                    }
+                ]
+            }
+        }
     }
 </script>
 

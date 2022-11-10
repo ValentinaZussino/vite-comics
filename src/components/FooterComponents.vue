36dd2b3fc61b07ba8d2175939294af39d873a3fc
ValentinaZussino <vzussino@protonmail.com>
2022-11-10 18:37:42 +0100

    impostaz liste footer

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,7 +1,24 @@
 <template>
     <footer >
         <div class="top-footer">
-            <div class="container"></div>
+            <div class="container">
+                <div class="col">
+                    <span>dc comics</span>
+                    <ul>
+                        <li v-for="(link, index) in linksComics" :key="index">
+                            <a href="#">{{link.text}}</a>
+                        </li>
+                    </ul>
+                    <span>shop</span>
+                    <ul>
+                        <li v-for="(shop, index) in linksShop" :key="index">
+                            <a href="#">{{shop.text}}</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
         </div>
         <div class="bottom-footer">
             <div class="container"></div>
@@ -12,6 +29,69 @@
 <script>
     export default {
     name: "FooterComponent",
+    data(){
+            return {
+                linksComics: [
+                    {
+                        text: "Characters",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Comics",
+                        url: "#",
+                        current: true,
+                    },
+                    {
+                        text: "Movies",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "TV",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Games",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Collectibles",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Videos",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Fans",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "News",
+                        url: "#",
+                        current: false,
+                    },
+                ],
+                linksShop: [
+                    {
+                        text: "Shop DC",
+                        url: "#",
+                        current: false,
+                    },
+                    {
+                        text: "Shop DC Collectibles",
+                        url: "#",
+                        current: false,
+                    },
+                ]
+            }
+        }
 }
 </script>
 
@@ -20,7 +100,23 @@
     .top-footer {
         background-image: url('../assets/img/footer-bg.jpg'); 
         background-size: cover;
-        height: 300px;
+        min-height: 300px;
+
+        .col {
+            // width: 100px;
+            margin-right: 20px;
+            border: 1px solid red;
+
+            span{text-transform: uppercase; color: white;}
+            ul{ 
+                list-style: none;
+                padding-top: 13px;
+                li{
+                    padding-bottom: 5px;
+                    a{text-decoration: none; color: $lightgray; font-size: 13px;}
+                }
+            }
+        }
     }
     .bottom-footer {
         background-color: $darkgray;
